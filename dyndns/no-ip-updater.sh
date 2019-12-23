bad57deaa651b3ee2b06f938d12a245867b502e7
daniel <38365646+dmz-madrid@users.noreply.github.com>
2019-12-23 13:40:04 +0100

    Update no-ip-updater.sh

--- dyndns/no-ip-updater.sh
+++ dyndns/no-ip-updater.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 
-# copy this to /usr/sbin:
-# sudo cp ddupdate.sh /usr/sbin/no-ip-updater.sh
-# this script uses wget to update no-ip dynamic dns
-USERNAME=""
-PASSWORD=""
-HOSTNAME=""
-LOCATION="/var/log/no-ip" # logs location /var/log/no-ip.log & /var/log/no-ip/oldip.txt
-USERAGENT="no-ip shell script/OS 1.0 maintainer-username@mail.com"
 
+# this script uses wget to update no-ip dynamic dns
+
+USERNAME="youruser"
+PASSWORD="yourpass"
+HOSTNAME="example.ddns.net"
+LOCATION="/var/log/no-ip" # logs location /var/log/no-ip.log & /var/log/no-ip/oldip.txt
+USERAGENT="no-ip shell script/O.S. 1.0 maintainer-username@mail.com"
+BASE64AUTH=$(echo '"$USERNAME:$PASSWORD"' | base64)
+AUTHORIZATION="Authorization: $BASE64AUTH"
 DATE=$(date +%Y-%m-%d\ %H:%M:%S)
 echo $DATE
 IP=`wget -q -O - http://www.ddnss.de/meineip.php| grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'`
-echo $IP
+
 
 # checking if log files created and credentials entered...
 
 if [ -z "$USERNAME" ]; then # -z string is null, that is, has zero length
-	echo "No user was set."
+	echo "No user was set. Use -u=username"
 	exit 10
 fi
 
 if [ ! -f "$LOCATION/no-ip.log" ] || [ ! -f "$LOCATION/oldip.log" ]; then #  -f filename - Check for regular file existence not a directory
 	echo "Log file and/or old ip file not found. Please create $LOCATION/no-ip.log and $LOCATION/oldip.log"
-	exit 10
+	exit	10
 else
 	OLDIP=`cat $LOCATION/oldip.log`
 	echo "Current IP=$OLDIP"
@@ -39,9 +40,10 @@ else
 	echo "$DATE -  New-IP: $IP / Old-IP: $OLDIP - UPDATE!"
 	echo "$DATE -  New-IP: $IP / Old-IP: $OLDIP - UPDATE!" >> $LOCATION/no-ip.log
 	echo $IP > $LOCATION/oldip.log
-	wget -q -O - --user-agent="$USERAGENT" 'https://'$USERNAME':'$PASSWORD'@dynupdate.no-ip.com/nic/update?hostname='$HOSTNAME'&myip='$IP''>> $LOCATION/no-ip.log
+	wget -q -O - "--header=\"$AUTHORIZATION\"" "--user-agent=\"$USERAGENT\"" 'https://'$USERNAME':'$PASSWORD'@dynupdate.no-ip.com/nic/update?hostname='$HOSTNAME'&myip='$IP''>> $LOCATION/no-ip.log
 	echo " " >> $LOCATION/no-ip.log
 	echo "Update ..."
+	echo '-q' '-O' '-' "--header=\"$AUTHORIZATION\"" "--user-agent=\"$USERAGENT\"" 'https://****' >> $LOCATION/no-ip.log
 	find $LOCATION/no-ip.log -type f -size +1M -exec rm -f {} \;
 fi
 exit 0

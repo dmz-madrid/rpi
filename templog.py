4c7a26b21e870b44ac8340eb79c373c6e9e787a7
daniel <38365646+dmz-madrid@users.noreply.github.com>
2019-12-24 22:14:16 +0100

    Delete templog.py

--- templog.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-from gpiozero import CPUTemperature
-import datetime
-
-now = datetime.datetime.now()
-r=CPUTemperature()
-
-print(str(now.strftime("%Y-%m-%d\t%H:%M\t"))+str(r.temperature)+'\'C')

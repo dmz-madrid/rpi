cff0d50dbd0129319625cd54593929df9b2eeb5b
daniel <38365646+dmz-madrid@users.noreply.github.com>
2019-12-24 22:17:11 +0100

    Update templog.sh

--- temperature/templog.sh
+++ temperature/templog.sh
@@ -1,1 +1,6 @@
 #!/usr/bin/bash
+
+LOCATION="/var/log/temperature" 									# the logs location /var/log/no-ip.log & /var/log/no-ip/oldip.txt
+TEMP=`/opt/vc/bin/vcgencmd measure_temp`          # temp
+DATE=$(date +%Y-%m-%d\ %H:%M:%S)                  # date and time
+echo "$DATE - $TEMP" >> $LOCATION/temp.log

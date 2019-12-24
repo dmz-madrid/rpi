7365a16e31c02bc498f00e2a4ff9d3ea0a41b87a
daniel <38365646+dmz-madrid@users.noreply.github.com>
2019-12-24 22:23:56 +0100

    Update templog.sh

--- temperature/templog.sh
+++ temperature/templog.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 LOCATION="/var/log/temperature"                   # the logs location /var/log/no-ip.log & /var/log/no-ip/oldip.txt
 TEMP=`/opt/vc/bin/vcgencmd measure_temp`          # temp
